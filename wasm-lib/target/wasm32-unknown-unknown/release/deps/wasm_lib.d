64abdc17b15ed6d7fa57dc1fead6df2f91cbd14f
samp42 <samuelproulx26@gmail.com>
2022-08-24 00:04:04 -0400

    remove pkg and target

--- wasm-lib/target/wasm32-unknown-unknown/release/deps/wasm_lib.d
+++
@@ -1,5 +0,0 @@
-/Users/samuelproulx/github/armulator/wasm-lib/target/wasm32-unknown-unknown/release/deps/wasm_lib.wasm: src/lib.rs
-
-/Users/samuelproulx/github/armulator/wasm-lib/target/wasm32-unknown-unknown/release/deps/wasm_lib.d: src/lib.rs
-
-src/lib.rs:
--- wasm-lib/target/wasm32-unknown-unknown/release/wasm_lib.d
+++
@@ -1,1 +0,0 @@
-/Users/samuelproulx/github/armulator/wasm-lib/target/wasm32-unknown-unknown/release/wasm_lib.wasm: /Users/samuelproulx/github/armulator/wasm-lib/src/lib.rs
