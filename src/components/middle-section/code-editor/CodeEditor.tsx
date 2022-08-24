8688c23e1a0799ef5055debc563a750fb7586deb
Vincent De Cooman <vincent.decooman123@gmail.com>
2022-08-23 21:45:02 -0400

    Get value from editor

--- src/components/middle-section/code-editor/CodeEditor.tsx
+++ src/components/middle-section/code-editor/CodeEditor.tsx
@@ -1,24 +1,36 @@
 import Editor from "@monaco-editor/react";
+import {useEffect, useState} from "react";
+import init, {add, parse} from "wasm-lib";
 
 export function CodeEditor() {
-    // const [value, setValue] = useState(code || "");
-    //
-    // const handleEditorChange = (value) => {
-    //     setValue(value);
-    //     onChange("code", value);
-    // };
+    const [value, setValue] = useState("// type your code...");
+    const handleEditorChange = (value: any) => {
+        setValue(value);
+    };
+
+    useEffect(() => {
+        init()
+    }, []);
+
+    function test() {
+        const data = value.split('\n').filter(r => !r.startsWith('//'))[0];
+        console.log(parse(data))
+    }
 
     return (
         <div>
-            <Editor
-                height="100vh"
-                width={`100%`}
-                language={"cpp"}
-                theme={"vs-dark"}
-                // value={value}
-                defaultValue="// some comment"
-                // onChange={handleEditorChange}
-            />
+            <button onClick={test}>Test</button>
+            <div>
+                <Editor
+                    height="100vh"
+                    width={`100%`}
+                    language={"cpp"}
+                    theme={"vs-dark"}
+                    value={value}
+                    defaultValue="// some comment"
+                    onChange={handleEditorChange}
+                />
+            </div>
         </div>
     );
 }
